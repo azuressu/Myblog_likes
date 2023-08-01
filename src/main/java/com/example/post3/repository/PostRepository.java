8c0de2b24392d1cc660e0d69aa57285b4fc1e4bd
azuressu <martina0130@naver.com>
2023-08-01 10:46:49 +0900

    QueryDSL, Pageable 사용 추가 commit

--- src/main/java/com/example/post3/repository/PostRepository.java
+++ src/main/java/com/example/post3/repository/PostRepository.java
@@ -1,11 +1,15 @@
 package com.example.post3.repository;
 
 import com.example.post3.entity.Post;
+import com.example.post3.query.PostRepositoryQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.RepositoryDefinition;
 
 import java.util.List;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+@RepositoryDefinition(domainClass = Post.class, idClass = Long.class)
+public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryQuery {
 
     // 모든 메모들을 수정한 시간을 기준으로 정렬
     List<Post> findAllByOrderByCreateTimeDesc();
