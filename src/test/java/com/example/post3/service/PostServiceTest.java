daf378f87589e2440183c6252984a54c54b79ded
azuressu <martina0130@naver.com>
2023-08-02 20:48:27 +0900

    Service 테스트 코드 수정 및 추가

--- src/test/java/com/example/post3/service/PostServiceTest.java
+++ src/test/java/com/example/post3/service/PostServiceTest.java
@@ -9,27 +9,25 @@ import com.example.post3.repository.PostRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
 
-/* Junit4 ver Test */
-@RunWith(MockitoJUnitRunner.class)
-@TestInstance(TestInstance.Lifecycle.PER_METHOD)
+/* Junit5 ver Test */
+@ExtendWith(MockitoExtension.class)
+// @TestInstance(TestInstance.Lifecycle.PER_METHOD)
 /* TestInstance.Lifecycle.PER_METHOD: 각 테스트 메서드마다 새로운 인스턴스를 생성하도록 지정하는 옵션 */
-class PostServiceImplTest {
+class PostServiceTest {
 
     /* Mock 객체를 테스트 대상 객체에 주입할 수 있고, 해당 객체의 메서드를 호출해 테스트 수행 가능 */
     @InjectMocks
@@ -43,7 +41,8 @@ class PostServiceImplTest {
 
     @BeforeEach
     void init() {
-        MockitoAnnotations.initMocks(this);
+        MockitoAnnotations.openMocks(this);
+        postService = new PostServiceImpl(postRepository, postLikeRepository);
     }
 
     @Test
@@ -66,9 +65,10 @@ class PostServiceImplTest {
         User user = new User();
         PostRequestDto postRequestDto = new PostRequestDto("title", "contents");
         var newPost = Post.builder().requestDto(postRequestDto).user(user).build();
-        given(postRepository.findById(20L)).willReturn(Optional.ofNullable(newPost));
+        newPost.setId(20L);
 
         // when
+        when(postRepository.findById(any(Long.class))).thenReturn(Optional.ofNullable(newPost));
         PostResponseDto postResponseDto = postService.getOnePost(20L);
 
         // then
@@ -83,16 +83,16 @@ class PostServiceImplTest {
         User user = new User();
         PostRequestDto postRequestDto = new PostRequestDto("title", "contents");
         var newPost = Post.builder().requestDto(postRequestDto).user(user).build();
-        given(postRepository.save(any())).willReturn(newPost);
+        when(postRepository.save(any(Post.class))).thenReturn(newPost);
 
         // when
-//        PostResponseDto postResponseDto = postService.createPost(postRequestDto, user);
-        postService.createPost(postRequestDto, user);
+        PostResponseDto postResponseDto = postService.createPost(postRequestDto, user);
+//        postService.createPost(postRequestDto, user);
 
         // then
-//        then(newPost.getTitle()).equals(postResponseDto.getTitle());
-//        then(newPost.getContents()).equals(postResponseDto.getContents());
-        then(postRepository).should(times(1)).save(any());
+        then(newPost.getTitle()).equals(postResponseDto.getTitle());
+        then(newPost.getContents()).equals(postResponseDto.getContents());
+//        then(postRepository).should(times(1)).save(any());
 
         /* 주석 처리 된 내용끼리 테스트 통과 확인 */
     }
@@ -122,13 +122,14 @@ class PostServiceImplTest {
         User user = new User();
         PostRequestDto postRequestDto = new PostRequestDto("title", "content");
         var newPost = Post.builder().requestDto(postRequestDto).user(user).build();
-        given(postLikeRepository.findByPostId(newPost.getId())).willReturn(null);
+        when(postLikeRepository.findByPostId(any(Long.class))).thenReturn(null);
 
         // when
+        Integer size = (int) postRepository.count();
         postService.deletePost(newPost, user);
 
         // then
-        then(15).equals(postRepository.count());
+        then(size).equals(postRepository.count());
     }
 
 }
